3e6da54c10865ab80322f17dc3c008971721d0f1
Kiyoshika <zachlweaver00@gmail.com>
2022-11-02 20:24:05 -0400

    finished all tokenizer tests

--- test/tokenizer/forloops.c
+++ test/tokenizer/forloops.c
@@ -4,23 +4,20 @@
 #include "token_array.h"
 #include "source_buffer.h"
 
-#define N_TEST_TOKENS 7 // number of tokens per test line
-#define N_TESTS 4 // number of test lines
+#define N_TEST_TOKENS 14 // number of tokens per test line
+#define N_TESTS 2 // number of test lines
 
 #include "test_tokens.c"
 
 int main()
 {
 	char expected_tokens[N_TEST_TOKENS * N_TESTS][10] = {
-		// we have four different variations of the below tokens
-		"int32", "x", "=", "12", "+", "1", ";",
-		"int32", "x", "=", "12", "+", "1", ";",
-		"int32", "x", "=", "12", "+", "1", ";",
-		"int32", "x", "=", "12", "+", "1", ";"
+		"for", "(", "int32", "x", "=", "0", ";", "x", "<=", "5", ";", "++", "x", ")",
+		"for", "(", "int32", "x", "=", "0", ";", "x", "<=", "5", ";", "++", "x", ")",
 	};
 
 	struct source_buffer_t srcbuffer;
-	srcbuffer_read("./assignment.moca", &srcbuffer);
+	srcbuffer_read("./forloops.moca", &srcbuffer);
 
 	struct token_array_t array;
 	tknzer_extract_tokens(
